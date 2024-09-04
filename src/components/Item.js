69673ff57b3fe0025cea8af88bb12cebeed36951
YasserEsam <yaaser10esam1010@gmail.com>
2024-09-04 22:57:37 +0300

    pass the test and add all states to the app

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isAdded , setIsAdded] = useState(false)
+  const handleAddToCart = () => {
+    setIsAdded(!isAdded)
+  }
+
+  const customeClass = isAdded ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={customeClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>{isAdded ? "In Cart" : "Add to Cart"}</button>
     </li>
   );
 }
